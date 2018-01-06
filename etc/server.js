26180d3441a5d48db6023d82f8ef9b94f826e66c
Yasin <425219856@qq.com>
2018-01-06 10:22:06 +0800

    1022

--- etc/server.js
+++ etc/server.js
@@ -1,14 +1,14 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
+http.createServer(function(request, response) {
 
     // 发送 HTTP 头部 
     // HTTP 状态值: 200 : OK
     // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
 
     // 发送响应数据 "Hello World"
-    response.end('Hello World\n');
+    response.end('Hello 阿斯顿撒大所\n');
 }).listen(8888);
 
 // 终端打印如下信息
